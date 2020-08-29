14366fbdc082c6ae935a2df47a13e37840033cb4
OmarAliSoliman <omarasmar9955@gmail.com>
2020-08-29 17:32:27 +0200

    animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+  AOS.init();
+
   if($('.header-slider').length){
     $(".header-slider").slick({
       dots: true,
@@ -15,91 +17,6 @@ $(document).ready(function () {
       rtl: true,
     })
   }
-
-  // choses-slider
-  // $('.choses-slider').slick({
-  //   dots: false,
-  //   infinite: false,
-  //   speed: 300,
-  //   slidesToShow: 5,
-  //   slidesToScroll: 1,
-  //   autoplay: false,
-  //   loop: true,
-  //   infinite: true,
-  //   responsive: [{
-  //       breakpoint: 1024,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 3,
-  //         infinite: true,
-  //         dots: true
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 800,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 1
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 1
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 376,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1
-  //       }
-  //     }
-  //   ]
-  // });
-
-  // $('.product-slider').slick({
-  //   dots: true,
-  //   infinite: false,
-  //   speed: 300,
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   autoplay: true,
-  //   loop: true,
-  //   infinite: true,
-  //   responsive: [{
-  //       breakpoint: 1024,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 3,
-  //         infinite: true,
-  //         dots: true
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 800,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 1
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 370,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1
-  //       }
-  //     }
-  //   ]
-  // });
 
   $('.more-slider').slick({
     dots: true,
